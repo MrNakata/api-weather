c3a3d797fd95fbb281992c0bcbedddd9a5fdfe8c
Manathan <manathan.tan@gmail.com>
2023-01-09 23:59:20 +0100

    + add addCityWeather()

--- cities.php
+++ cities.php
@@ -51,19 +51,6 @@ function getCities (int $cityId = null):array {
     }
     return $tDatas;
 }
-/**
- * Ajout d'une nouvelle ville
- */
-function addCity () {
-    global $pdo;
-    $country = $_POST['country'];
-    $label   = $_POST['label'];
-    $q       = "INSERT INTO city (country, city_label, CREATION_DATE) VALUES (:country, :city_label: NOW())";
-    $insert  = $pdo->prepare($q);
-    $insert->bindValue(":country", $country, PDO::PARAM_STR);
-    $insert->bindValue(":city_label", $label, PDO::PARAM_STR);
-    $insert->execute();
-}
 
 /**
  * Récup la météo d'une ville
@@ -90,6 +77,40 @@ function getCityWeather (int $cityId):array {
     }
     return $tDatas;
 }
+/**
+ * Ajout d'une nouvelle ville
+ */
+function addCity () {
+    global $pdo;
+    $country = $_POST['country'];
+    $label   = $_POST['label'];
+    $q       = "INSERT INTO city (country, city_label, CREATION_DATE) VALUES (:country, :city_label: NOW())";
+    $insert  = $pdo->prepare($q);
+    $insert->bindValue(":country", $country, PDO::PARAM_STR);
+    $insert->bindValue(":city_label", $label, PDO::PARAM_STR);
+    $insert->execute();
+}
+/**
+ * Ajout d'une nouvelle ville
+ * @param int $cityId
+ */
+function addCityWeather (int $cityId) {
+    global $pdo;
+    $temperature   = $_POST['temperature'];
+    $weather       = $_POST['weather'];
+    $precipitation = $_POST['precipitation'];
+    $humidity      = $_POST['humidity'];
+    $wind          = $_POST['wind'];
+    $q = "INSERT INTO weather (city_id, temperature, weather, precipitation, humidity, wind, date) VALUES (:city_id, :temperature, :weather, :precipitation, :humidity, :wind, NOW())";
+    $insert  = $pdo->prepare($q);
+    $insert->bindValue(":city_id", $cityId, PDO::PARAM_STR);
+    $insert->bindValue(":temperature", $temperature, PDO::PARAM_STR);
+    $insert->bindValue(":weather", $weather, PDO::PARAM_STR);
+    $insert->bindValue(":precipitation", $precipitation, PDO::PARAM_STR);
+    $insert->bindValue(":humidity", $humidity, PDO::PARAM_STR);
+    $insert->bindValue(":wind", $wind, PDO::PARAM_STR);
+    $insert->execute();
+}
 /*
     INSTRUCTIONS
  */
@@ -105,7 +126,12 @@ switch($_SERVER["REQUEST_METHOD"]) {
         break;
 
     case 'POST':
-        addCity();
+        $cityId = getUrlId('cities');
+        if (strpos($_SERVER['REQUEST_URI'], "weather") !== false) {
+            addCityWeather($cityId);
+        } else {
+            addCity();
+        }
         break;
 
     default:
